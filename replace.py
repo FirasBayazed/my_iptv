571507b20672351d33a382fe1d313715431ee97b
Firas Bayazed <firas_bayazed@hotmail.com>
2020-06-15 17:59:56 +0200

    add make groups file to guide you how to sort the channels in groups

--- replace.py
+++ replace.py
@@ -1,7 +1,7 @@
-with open("osn.txt", encoding="utf8") as f:
+with open("data_out_new1.txt", encoding="utf8") as f:
     content = f.read();
 
 content = content.replace("http", "\nhttp");
 
-with open("filename.txt", mode="w", encoding="utf8") as f:
+with open("filename1.txt", mode="w", encoding="utf8") as f:
     content = f.write(content);
--- replace1.py
+++ replace1.py
@@ -1,7 +1,7 @@
-with open("filename.txt", encoding="utf8") as f:
+with open("filename1.txt", encoding="utf8") as f:
     content = f.read();
 
 content = content.replace("#EXTINF", "\n#EXTINF");
 
-with open("filename1.txt", mode="w", encoding="utf8") as f:
+with open("filename2.txt", mode="w", encoding="utf8") as f:
     content = f.write(content);
